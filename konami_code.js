d17aac9a2d33c66ec042895adea8211f829171d1
JStern95 <JSternberg95@gmail.com>
2018-10-14 22:41:59 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,20 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0;
+
+  document.body.addEventListener('keydown', function(e) {
+    const key = event.key
+
+    if (codes[index] === key) {
+      index++;
+    } else {
+      index = 0
+    }
+
+    if (index === codes.length) {
+      window.alert('Woah!');
+      index = 0
+    }
+  })
 }
